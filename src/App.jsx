35fa2ff75ea53ca77831594400f26a39f8d88a9f
ebad sheikh <ibbeescode@gmail.com>
2026-04-21 02:23:24 +0500

    Refactor App component to handle error state and improve loading indication

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import {useState, useEffect} from "react"
 import Loader from "./components/loader"
 import BookList from "./components/booklist"
-
+import ErrorMessage from "./components/errormessage"
 
 
 function App() {
   const [books, setBooks] = useState([])
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
 
   useEffect(() => {
     async function fetchBook(){
       try{
+
+        setLoading(true)
+        setError("")
+
         const res = await fetch("https://openlibrary.org/search.json?q=harry+potter")
+        if(!res){
+          throw new Error("Failed to fetch books")
+        }
+        
         const data = await res.json()
         setBooks(data.docs)
       }
       catch(err){
-        console.log("error fetching books:", err)
+        setError(err.message)
       }
       finally{
         setLoading(false)
@@ -27,8 +36,15 @@ function App() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      { loading ? <Loader/> : <BookList books={books} /> }
+    <div className="p-6 max-w-5x1 mx-auto">
+      {loading && <Loader/>}
+      {error && <ErrorMessage message={error}/>}
+      {!loading && !error && books.length === 0 && (
+        <p className="text-gray-600 text-center nt-10">N0 books found</p>
+      )}
+      {!loading && !error && books.length > 0 &&(
+        <BookList books={books}/>
+      )}
     </div>
   );
 }
